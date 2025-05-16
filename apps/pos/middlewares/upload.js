90f0b76654efbe1cfb77e63942449659a6d15dd4
vm-rpos <vm.rpos@gmail.com>
2025-05-16 11:52:13 +0530

    Create upload.js

---
+++ apps/pos/middlewares/upload.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads/"),
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (["image/jpeg", "image/png", "image/jpg"].includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only jpeg, jpg, png files allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  fileFilter,
+});
+
+module.exports = upload;
