2e3ca4b761729879aee3bdee185aa1bca4084a87
vm-rpos <vm.rpos@gmail.com>
2025-08-27 18:44:46 +0530

    Update emailUtils.js

--- utils/emailUtils.js
+++ utils/emailUtils.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
